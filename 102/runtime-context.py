f5098d2ca31584e47d051db5003c488c4f99c3bc
Brian Roepke <broepke@gmail.com>
2024-12-17 09:12:50 -0600

    Added additional print steps

--- 102/runtime-context.py
+++ 102/runtime-context.py
@@ -5,6 +5,7 @@ from prefect import runtime
 @flow(log_prints=True)
 def my_flow(x):
     print("My name is", runtime.flow_run.name)
+    print(x)
     print("I belong to deployment", runtime.deployment.name)
     my_task(2)
 
@@ -12,6 +13,7 @@ def my_flow(x):
 @task
 def my_task(y):
     print("My name is", runtime.task_run.name)
+    print(y)
     print("Flow run parameters:", runtime.flow_run.parameters)
 
 
